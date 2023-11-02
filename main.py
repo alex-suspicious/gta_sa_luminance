04ba2f359618ae4b17aecf85071770fbd9f754eb
alex <savav.col@gmail.com>
2023-11-02 17:35:43 +0600

    d3d8 changed to d3d8.dll

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def install():
     url = "https://github.com/crosire/d3d8to9/releases/download/v1.12.0/d3d8.dll"
     response = requests.get(url, stream=True)
 
-    with open("d3d8", "wb") as handle:
+    with open("d3d8.dll", "wb") as handle:
         for data in tqdm(response.iter_content()):
             handle.write(data)
 
